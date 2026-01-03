6e11dc688bead95f53626fb0bb8f47cf5a252282
lendrik-kumar <sanketkamboj2006@gmail.com>
2026-01-03 12:23:14 +0530

    basic setup for 3d created, models to be added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { KeyboardControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
 
-const App = () => {
+const keyboardMap = [
+  { name: "forward", keys: ["ArrowUp", "KeyW"] },
+  { name: "backward", keys: ["ArrowDown", "KeyS"] },
+  { name: "left", keys: ["ArrowLeft", "KeyA"] },
+  { name: "right", keys: ["ArrowRight", "KeyD"] },
+  { name: "run", keys: ["Shift"] },
+];
+
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <KeyboardControls map={keyboardMap}>
+      <Canvas
+        shadows
+        camera={{ position: [3, 3, 3], near: 0.1, fov: 40 }}
+        style={{
+          touchAction: "none",
+        }}
+      >
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
+    </KeyboardControls>
+  );
 }
 
-export default App
+export default App;
